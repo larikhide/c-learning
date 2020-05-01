1b0ac29337cc26119dc74e74e0c38d20971d137c
Peter Ustyuzhanin <petrakliy@gmail.com>
2020-05-01 10:31:16 +0300

    added first if

---
+++ 2-if/Terms.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, const char* argv[]) {
+    char answer;
+    printf("Do you want mw to salute you? (y/n)");
+    scanf("%c", &answer);
+    if(answer == 'y') {
+        printf("Hello, user!");
+    } else if (answer == 'n'){
+        printf("I didnt want to salute you anyway!");
+    } else {
+        printf("I cant understand your input!");
+    }
+
+        return 0;
+}
